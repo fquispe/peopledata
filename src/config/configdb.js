99d7f5a6edabda853c93c86f97e45a0c5bd6ae59
fquispe <ferquis82@gmail.com>
2019-10-26 19:19:01 -0500

    modifie file config db

--- src/config/configdb.js
+++ src/config/configdb.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 module.exports = () => {
     return mysql.createConnection({
-      host: 'localhost',
+      host: process.env.DATABASE_HOST,
       user: 'root',
       port: '3306',
       password: 'Mysql@2019',
